daeeaae8f923c14438cd5c4feffd5d89222ab212
Martin Angers <martin.n.angers@gmail.com>
2016-07-20 09:16:09 -0400

    turtles: add PrintfLogger adapter

--- turtles.go
+++ turtles.go
@@ -2,13 +2,35 @@
 // handlers.
 package turtles
 
-import "net/http"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
 
 // Logger defines the Log method that is used to log structured
 // data, in tuples of alternating keys/values. The go-kit logger
 // satisfies this interface (github.com/go-kit/kit/log).
 type Logger interface {
 	Log(...interface{}) error
+}
+
+// PrintfLogger is an adapter to use Printf-style functions as a
+// Logger in the middlewares that accept one. For example,
+// the stdlib's log.Printf function can be used via this adapter.
+type PrintfLogger func(string, ...interface{})
+
+// Log implements Logger for the PrintfLogger function adapter.
+func (fn PrintfLogger) Log(args ...interface{}) error {
+	var buf bytes.Buffer
+	for i := 0; i < len(args)-1; i++ {
+		if i > 0 {
+			buf.WriteByte(' ')
+		}
+		fmt.Fprintf(&buf, "%s=%q", args[i], args[i+1])
+	}
+	fn(buf.String())
+	return nil
 }
 
 // Wrapper defines the Wrap method required to build a middleware-style
