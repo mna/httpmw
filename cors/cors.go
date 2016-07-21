d7593c1819b59729f89549537706fdfb940c6fdd
Martin Angers <martin.n.angers@gmail.com>
2016-07-21 08:35:48 -0400

    cors: small doc change

--- cors/cors.go
+++ cors/cors.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package cors implements a CORS middleware and handler for OPTIONS requests.
+// Package cors implements a CORS middleware and a handler
+// for OPTIONS requests.
 package cors
 
 import (
