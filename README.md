d5beb8e1a5f4e9f313a296201779a9e8d5900722
Martin Angers <martin.n.angers@gmail.com>
2016-07-20 22:24:04 -0400

    timeout: add middleware test

---
+++ README.md
@@ -0,0 +1,4 @@
+# turtles - a collection of bite-sized middleware and chaining support
+
+It's 🐢  🐢  turtles all the way down.
+
---
+++ timeout/timeout_test.go
@@ -0,0 +1,41 @@
+// Copyright 2016 Martin Angers. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package timeout
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/PuerkitoBio/turtles"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimeout(t *testing.T) {
+	cases := []struct {
+		sleep   string
+		timeout string
+		want    int
+	}{
+		{"10ms", "20ms", 200},
+		{"10ms", "5ms", 503},
+		{"10ms", "0", 503},
+	}
+	for i, c := range cases {
+		dur, _ := time.ParseDuration(c.timeout)
+		to := &Timeout{Duration: dur}
+		h := turtles.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			dur, _ := time.ParseDuration(c.sleep)
+			time.Sleep(dur)
+			w.WriteHeader(200)
+		}), to)
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("", "/", nil)
+		h.ServeHTTP(w, r)
+
+		assert.Equal(t, c.want, w.Code, "%d: status", i)
+	}
+}
